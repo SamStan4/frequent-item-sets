ce0199afe8ccc77636caabd7dd2bee942b962119
Samuel Stanley <sam.charles.stanley@gmail.com>
2024-05-16 01:02:31 -0700

    added object oriented approach and gitignore

--- program.py
+++ program.py
@@ -1,39 +1,249 @@
 #!/usr/bin/python3
 
-def get_triangle_matrix(num_elements : int) -> list[int]:
-    return [0] * int((((num_elements - 1) * num_elements) / 2))
+data_file_name = "browsing-data.txt"
+given_threshold = 100
 
-def get_num_freq_items(threashold : int, item_freq_table : dict) -> int:
-    num_freq_items = 0
-    for item in item_freq_table:
-        if item_freq_table[item] > threashold:
-            num_freq_items += 1
-    return num_freq_items
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#     method name     --> 
+#     method purpose  --> 
+#     member of       -->
+#     preconditions   --> 
+#     postconditions  --> 
+#     date created    --> 
+#     last modified   --> 
+#     programmer      --> 
+#     sources         --> 
 
-def get_freq_item_list(threashold : int, item_freq_table : dict) -> list[str]:
-    freq_items = []
-    for item in item_freq_table:
-        if item_freq_table[item] > threashold:
-            freq_items.append(item)
-    return freq_items
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-def print_freq_items(threashold : int, item_freq_table : dict) -> None:
-    i = 0
-    j = 0
-    for item in item_freq_table:
-        if item_freq_table[item] > threashold:
-            print("ITEM[" + str(item) + "] : FREQ[" + str(item_freq_table[item]) + "]")
+#################################################################################################################################################################################################################
+#
+#          /$$$$$$  /$$        /$$$$$$   /$$$$$$   /$$$$$$  /$$$$$$$$  /$$$$$$ 
+#         /$$__  $$| $$       /$$__  $$ /$$__  $$ /$$__  $$| $$_____/ /$$__  $$
+#        | $$  \__/| $$      | $$  \ $$| $$  \__/| $$  \__/| $$      | $$  \__/
+#        | $$      | $$      | $$$$$$$$|  $$$$$$ |  $$$$$$ | $$$$$   |  $$$$$$ 
+#        | $$      | $$      | $$__  $$ \____  $$ \____  $$| $$__/    \____  $$
+#        | $$    $$| $$      | $$  | $$ /$$  \ $$ /$$  \ $$| $$       /$$  \ $$
+#        |  $$$$$$/| $$$$$$$$| $$  | $$|  $$$$$$/|  $$$$$$/| $$$$$$$$|  $$$$$$/
+#         \______/ |________/|__/  |__/ \______/  \______/ |________/ \______/ 
+#
+#################################################################################################################################################################################################################
 
-def get_item_dict() -> dict:
-    input_file = open("browsing-data.txt", "r")
-    item_table = {}
-    for line in input_file:
-        codes = line.replace('\n', '').split()
-        for code in codes:
-            if code not in item_table:
-                item_table[code] = 1
-            else:
-                item_table[code] += 1
-    return item_table
+class triangle_matrix:
 
-get_triangle_matrix(642)
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    #     method name     --> __init__()
+    #     method purpose  --> constructor
+    #     member of       --> triangle_matrix
+    #     preconditions   --> none
+    #     postconditions  --> none
+    #     date created    --> 5/15/2024
+    #     last modified   --> 5/15/2024
+    #     programmer      --> sam stanley
+    #     sources         --> none
+
+    def __init__(self, item_list : list) -> None:
+        self.pair_grid = []
+        self.num_items = len(item_list)
+        self.code_to_num = {}
+        self.num_to_code = {}
+        size = len(item_list)
+        self.pair_grid = [0] * (size * (size + 1) // 2)
+        for i in range (0, len(item_list), 1):
+            self.code_to_num[item_list[i]] = i
+            self.num_to_code[i] = item_list[i]
+        # for i in range (len(self.pair_grid)):
+        #     self.pair_grid[i] = 0
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    #     method name     --> get_internal_index()
+    #     method purpose  --> returns the actual index of an element in the array, 2d -> 1d
+    #     member of       --> triangle_matrix
+    #     preconditions   --> none
+    #     postconditions  --> none
+    #     date created    --> 5/15/2024
+    #     last modified   --> 5/15/2024
+    #     programmer      --> sam stanley
+    #     sources         --> none
+
+    def get_internal_index(self, i : str, j : str) -> int:
+        i = self.code_to_num[i]
+        j = self.code_to_num[j]
+        if (i == j):
+            return -1
+        if (i > j):
+            i, j = j, i
+        return i * (2 * self.num_items - i + 1) // 2 + j - i
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+    def __getitem__(self, index : list) -> int:
+        i, j = index
+        real_index = self.get_internal_index(i, j)
+        if real_index == -1:
+            return -1
+        elif real_index >= len(self.pair_grid):
+            return -1
+        else:
+            return self.pair_grid[real_index]
+        
+    
+    def __setitem__(self, index : list, val : int) -> None:
+        i, j = index
+        real_index = self.get_internal_index(i, j)
+        if real_index == -1:
+            return
+        elif real_index >= len(self.pair_grid):
+            return
+        else:
+            self.pair_grid[real_index] = val
+
+    def get_num_items(self, min = 0) -> None:
+        total = 0
+        for i in range(len(self.pair_grid)):
+            if self.pair_grid[i] >= min:
+                total += 1
+        return total
+
+
+
+
+
+#################################################################################################################################################################################################################
+
+class analysis_wrapper:
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    #     method name     --> __init__()
+    #     method purpose  --> constructor
+    #     member of       --> analysis_wrapper
+    #     preconditions   --> none
+    #     postconditions  --> none
+    #     date created    --> 5/15/2024
+    #     last modified   --> 5/15/2024
+    #     programmer      --> sam stanley
+    #     sources         --> none
+
+    def __init__(self, min : int) -> None:
+        self.threshold = min
+        self.frequent_items = []
+        self.frequent_pairs = []
+        self.frequent_tripples = []
+        self.populate_frequent_items()
+        self.populate_frequent_pairs()
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    #     method name     --> run_analysis()
+    #     method purpose  --> runs the entire program
+    #     member of       --> analysis_wrapper
+    #     preconditions   --> none
+    #     postconditions  --> none
+    #     date created    --> 5/15/2024
+    #     last modified   --> 5/15/2024
+    #     programmer      --> sam stanley
+    #     sources         --> none
+
+    def run_analysis(self) -> None:
+        # self.print_frequent_items()
+        # self.print_frequent_pairs()
+        # print(len(self.frequent_pairs))
+        # print(len(self.frequent_items))
+        print()
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    #     method name     --> populate_frequent_items()
+    #     method purpose  --> populates a list of items that appear a minimum number of times
+    #     member of       --> analysis_wrapper
+    #     preconditions   --> program constructor was called, file exists
+    #     postconditions  --> none
+    #     date created    --> 5/15/2024
+    #     last modified   --> 5/15/2024
+    #     programmer      --> sam stanley
+    #     sources         --> none
+
+    def populate_frequent_items(self) -> None:
+        item_freq = {}
+        input_stream = open(data_file_name, "r")
+        for line in input_stream:
+            basket = line.replace('\n', '').split()
+            for item in basket:
+                if item in item_freq:
+                    item_freq[item] += 1
+                else:
+                    item_freq[item] = 1
+        input_stream.close()
+        for key in item_freq:
+            if item_freq[key] >= self.threshold:
+                self.frequent_items.append(key)
+    
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    #     method name     --> populate_frequent_items()           this function is slow AF
+    #     method purpose  --> populates a list of items that appear a minimum number of times
+    #     member of       --> analysis_wrapper
+    #     preconditions   --> program constructor was called, file exists
+    #     postconditions  --> none
+    #     date created    --> 5/15/2024
+    #     last modified   --> 5/16/2024
+    #     programmer      --> sam stanley
+    #     sources         --> none
+
+    def populate_frequent_pairs(self) -> None:
+        pair_matrix = triangle_matrix(self.frequent_items)
+        frequent_item_set = set(self.frequent_items)
+        input_stream = open(data_file_name, "r")
+        for line in input_stream:
+            basket = line.replace('\n', '').split(" ")
+            for i in range(len(basket)):
+                for j in range(i + 1, len(basket)):
+                    if (basket[i] in frequent_item_set) and (basket[j] in frequent_item_set):
+                        pair_matrix[basket[i], basket[j]] += 1
+        input_stream.close()
+        for i in range(len(self.frequent_items)):
+            for j in range(i + 1, len(self.frequent_items)):
+                if (pair_matrix[self.frequent_items[i], self.frequent_items[j]] > self.threshold):
+                    self.frequent_pairs.append([self.frequent_items[i], self.frequent_items[j]])
+        
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    #     method name     --> print_frequent_items()
+    #     method purpose  --> displays to the terminal, the list of frequent items
+    #     member of       --> analysis_wrapper
+    #     preconditions   --> frequent_items is populated
+    #     postconditions  --> none
+    #     date created    --> 5/15/2024
+    #     last modified   --> 5/15/2024
+    #     programmer      --> sam stanley
+    #     sources         --> none
+
+    def print_frequent_items(self) -> None:
+        for item in self.frequent_items:
+            print(item)
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+    
+    def print_frequent_pairs(self) -> None:
+        for pair in self.frequent_pairs:
+            print(pair)
+
+
+
+
+#################################################################################################################################################################################################################
+#
+#                                 /$$             /$$$ /$$$          /$$$$$$                                 /$$     /$$                    
+#                                |__/            /$$_/|_  $$        /$$__  $$                               | $$    |__/                    
+#         /$$$$$$/$$$$   /$$$$$$  /$$ /$$$$$$$  /$$/    \  $$      | $$  \__//$$   /$$ /$$$$$$$   /$$$$$$$ /$$$$$$   /$$  /$$$$$$  /$$$$$$$ 
+#        | $$_  $$_  $$ |____  $$| $$| $$__  $$| $$      | $$      | $$$$   | $$  | $$| $$__  $$ /$$_____/|_  $$_/  | $$ /$$__  $$| $$__  $$
+#        | $$ \ $$ \ $$  /$$$$$$$| $$| $$  \ $$| $$      | $$      | $$_/   | $$  | $$| $$  \ $$| $$        | $$    | $$| $$  \ $$| $$  \ $$
+#        | $$ | $$ | $$ /$$__  $$| $$| $$  | $$|  $$     /$$/      | $$     | $$  | $$| $$  | $$| $$        | $$ /$$| $$| $$  | $$| $$  | $$
+#        | $$ | $$ | $$|  $$$$$$$| $$| $$  | $$ \  $$$ /$$$/       | $$     |  $$$$$$/| $$  | $$|  $$$$$$$  |  $$$$/| $$|  $$$$$$/| $$  | $$
+#        |__/ |__/ |__/ \_______/|__/|__/  |__/  \___/|___/        |__/      \______/ |__/  |__/ \_______/   \___/  |__/ \______/ |__/  |__/
+#
+#################################################################################################################################################################################################################
+
+def main() -> None:
+    
+    program = analysis_wrapper(given_threshold)
+    program.run_analysis()
+
+#################################################################################################################################################################################################################
+
+main()
